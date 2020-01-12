803cc40e1b851642764f85290e4bf0c718a79e91
Mads <cph-mh752@cphbusiness.dk>
2020-01-12 02:38:10 +0100

    Initial setup ongoing

--- src/main/java/utils/EMF_Creator.java
+++ src/main/java/utils/EMF_Creator.java
@@ -118,7 +118,7 @@ public class EMF_Creator {
         if (isDeployed) {
             user = System.getenv("USER");
             pw = System.getenv("PW");
-            connection_str = System.getenv("CONNECTION_EksamPrepType1");
+            connection_str = System.getenv("CONNECTION_EksamPrepType2");
         }
         /*
         On your server in /opt/tomcat/bin/setenv.sh   add the following WITH YOUR OWN VALUES
